a2657e8ba36823dd0c984bd96241e8ec4f5fe246
LaceySana <129418829+LaceySana@users.noreply.github.com>
2025-09-09 18:45:43 -0600

    bom.js button event listeners added

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,12 +2,22 @@ const input = document.getElementById("favchap");
 const addButton = document.querySelector("button");
 const chapList = document.getElementById("list");
 
-const li = document.createElement("li");
-const delButton = document.createElement("button");
+addButton.addEventListener("click", () => {
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        const delButton = document.createElement("button");
+        
+        li.textContent = input.value;
+        delButton.textContent = "❌";
+        li.append(delButton);
+        chapList.append(li);
 
-li.innerHTML = input.value;
-delButton.innerHTML = "❌";
-
-li.append(delButton);
-
-chapList.append(li);
+        input.value = "";
+        
+        delButton.addEventListener("click", () => {
+            chapList.removeChild(li);
+            input.focus();
+        });
+    }
+    input.focus();
+});
