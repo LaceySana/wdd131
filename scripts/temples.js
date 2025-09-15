b210642e303eadeef0060852b94db058a706d383
LaceySana <129418829+LaceySana@users.noreply.github.com>
2025-09-15 14:23:57 -0600

    temples css updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,5 @@
+// FOOTER
+
 const currentyear = document.querySelector("#currentYear");
 
 const today = new Date();
@@ -8,3 +10,14 @@ const lastmodified = document.querySelector("#lastModified");
 
 const modifDate = document.lastModified;
 lastmodified.innerHTML = `Last Modification: ${modifDate}`;
+
+
+// HAMBURGER MENU
+
+const hamButton = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+    nav.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
